7cfc5910a6af86be1f4298bb1029e4f4a65ab4ff
jervis <jervis_qiang@aliyun.com>
2021-07-29 16:16:41 +0800

    cache

--- src/test/java/cn/muses/config/redisson/RedissonTest.java
+++ src/test/java/cn/muses/config/redisson/RedissonTest.java
@@ -1,10 +1,13 @@
 /*
-Copyright 2019 All rights reserved.
+ * Copyright 2019 All rights reserved.
  */
 
 package cn.muses.config.redisson;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RBucket;
 import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +20,21 @@ import cn.muses.config.JunitTest;
  */
 public class RedissonTest extends JunitTest {
 
-	@Autowired
-	private RedissonClient redisson;
+    @Autowired
+    private RedissonClient redisson;
 
-	@Test
-	public void RScoredSortedSetTest() {
-		RScoredSortedSet<String> sortedSet = redisson.getScoredSortedSet("RScoredSortedSetTest");
-		RScoredSortedSet<String> sortedSet1 = redisson.getScoredSortedSet("RScoredSortedSetTest:1");
-		System.out.println(sortedSet.add(300, "test"));
-		System.out.println(sortedSet1.add(300, "test"));
-	}
+    @Test
+    public void RScoredSortedSetTest() {
+        RScoredSortedSet<String> sortedSet = redisson.getScoredSortedSet("RScoredSortedSetTest");
+        RScoredSortedSet<String> sortedSet1 = redisson.getScoredSortedSet("RScoredSortedSetTest:1");
+        System.out.println(sortedSet.add(300, "test"));
+        System.out.println(sortedSet1.add(300, "test"));
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        final RBucket<Object> bucket = redisson.getBucket("key");
+        bucket.set("key", 10, TimeUnit.SECONDS);
+        System.out.println(bucket.remainTimeToLive());
+    }
 }
